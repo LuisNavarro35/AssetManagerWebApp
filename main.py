619d5982c1a8f2d84e3c098f23d57e093fbc9b1c
alberto Navarro <alberto.navarro.imt@gmail.com>
2024-09-08 19:53:08 -0500

    Create html templates

--- main.py
+++ main.py
@@ -13,5 +13,46 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/assign-asset')
+def assign_asset():
+
+    return render_template("assign_asset.html")
+
+
+@app.route('/move-group')
+def move_group():
+
+    return render_template("assetgroup.html")
+
+
+@app.route('/maintenance-event')
+def maintenance_event():
+
+    return render_template("createmaintenance.html")
+
+
+@app.route('/maintenance-history')
+def maintenance_history():
+
+    return render_template("maintenancehistory.html")
+
+
+@app.route('/new-asset')
+def new_asset():
+
+    return render_template("createasset.html")
+
+
+@app.route('/new-assetgroup')
+def new_assetgroup():
+
+    return render_template("creategroup.html")
+
+
+@app.route('/delet-data')
+def delete_data():
+
+    return render_template("deletedata.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
